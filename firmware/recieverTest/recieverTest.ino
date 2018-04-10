744daf0630582c7a1a21c60596b396b8dc8a1b43
Jasper Chan <jasperchan515@gmail.com>
2018-04-10 12:35:49 -0700

    Added test for controller

---
+++ firmware/recieverTest/recieverTest.ino
@@ -0,0 +1,82 @@
+byte last_channel_1, last_channel_2, last_channel_3, last_channel_4;
+volatile int receiver_input_channel_1, receiver_input_channel_2, receiver_input_channel_3, receiver_input_channel_4;
+unsigned long timer_channel_1, timer_channel_2, timer_channel_3, timer_channel_4, esc_timer, esc_loop_timer;
+unsigned long timer_1, timer_2, timer_3, timer_4, current_time;
+int receiver_input[5];
+
+void setup() {
+  PCICR  |= (1 << PCIE0);
+  PCMSK0 |= (1 << PCINT0);
+  PCMSK0 |= (1 << PCINT1);
+  PCMSK0 |= (1 << PCINT2);
+  PCMSK0 |= (1 << PCINT3);
+  Serial.begin(9600);
+}
+
+ISR(PCINT0_vect){
+  current_time = micros();
+  //Channel 1
+  if(PINB & B00000001){
+    if(last_channel_1 == 0){
+      last_channel_1 = 1;
+      timer_1 = current_time;
+    }
+  }
+  else if(last_channel_1 == 1){
+    last_channel_1 = 0;
+    receiver_input[1] = current_time - timer_1;
+  }
+  //Channel 2
+  if(PINB & B00000010 ){
+    if(last_channel_2 == 0){
+      last_channel_2 = 1;
+      timer_2 = current_time;
+    }
+  }
+  else if(last_channel_2 == 1){
+    last_channel_2 = 0;
+    receiver_input[2] = current_time - timer_2;
+  }
+  //Channel 3
+  if(PINB & B00000100 ){
+    if(last_channel_3 == 0){
+      last_channel_3 = 1;
+      timer_3 = current_time;
+    }
+  }
+  else if(last_channel_3 == 1){
+    last_channel_3 = 0;
+    receiver_input[3] = current_time - timer_3;
+
+  }
+  //Channel 4
+  if(PINB & B00001000 ){
+    if(last_channel_4 == 0){
+      last_channel_4 = 1;
+      timer_4 = current_time;
+    }
+  }
+  else if(last_channel_4 == 1){
+    last_channel_4 = 0;
+    receiver_input[4] = current_time - timer_4;
+  }
+}
+
+void print_signals(){
+  Serial.print("Channel 1:");
+  Serial.print(receiver_input_channel_1);
+  Serial.print("\t Channel 2:");
+  Serial.print(receiver_input_channel_2);
+  Serial.print("\t Channel 3:");
+  Serial.print(receiver_input_channel_3);
+  Serial.print("\t Channel 4:");
+  Serial.println(receiver_input_channel_4);
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(250);
+  print_signals();
+}
